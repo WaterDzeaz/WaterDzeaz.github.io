36e5cd8d2373dc8c72741b1bf5faf2c5a24b06e5
Mark Sloesen <marksloesen@marel.com>
2026-05-21 13:22:28 +0200

    Scaleable size

--- components/elements/head.js
+++ components/elements/head.js
@@ -1,25 +1,30 @@
 import RootScript from "../script/root.js";
 
 export default class HeadElement {
-  /**
-   *
-   * @param {RootScript} root
-   */
-  constructor(root) {
-    this.Root = root;
+    /**
+     *
+     * @param {RootScript} root
+     */
+    constructor(root) {
+        this.Root = root;
 
-    this.createHead();
-  }
+        this.createHead();
+    }
 
-  createHead() {
-    const css = document.createElement("link");
-    css.rel = "stylesheet";
-    css.href = "/style/stylesheet.css";
+    createHead() {
+        const title = document.createElement("title");
+        title.innerHTML = "Ginger de Graaff";
 
-    const title = document.createElement("title");
-    title.innerHTML = "Ginger de Graaff";
+        const icon = document.createElement("link");
+        icon.rel = "icon";
+        icon.href = "/images/favicon.ico";
 
-    this.Root.Head.appendChild(css);
-    this.Root.Head.appendChild(title);
-  }
+        const css = document.createElement("link");
+        css.rel = "stylesheet";
+        css.href = "/style/stylesheet.css";
+
+        this.Root.Head.appendChild(title);
+        this.Root.Head.appendChild(icon);
+        this.Root.Head.appendChild(css);
+    }
 }
