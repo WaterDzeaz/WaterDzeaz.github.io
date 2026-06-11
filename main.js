1f1990fd2b0d99e3ba7cac274fe342406fa55838
w1-sloesenmark <84622625+WaterDzeaz@users.noreply.github.com>
2026-06-12 00:16:43 +0200

    Final 8

--- main.js
+++ main.js
@@ -71,9 +71,6 @@ new BronnenElement(root, [
         "Oppenoorth, W. H. (2008). Effecten van psychiatrische stoornissen van ouders op ontwikkeling van kinderen. Tijdschrift Voor Psychiatrie. https://www.tijdschriftvoorpsychiatrie.nl/nl/artikelen/article/50-6524_Effecten-van-psychiatrische-stoornissen-van-ouders-op-ontwikkeling-van-kinderen"
     ),
     new BronItem("Richtlijnen Jeugdhulp en Jeugdbescherming. (2025, 20 juni). Ingrijpende gevolgen | Richtlijn Kinderen van ouders met psychische en/of verslavingsproblemen. Geraadpleegd op 19 april 2026, van https://www.richtlijnenjeugdhulp.nl/koppkov/3-oorzaken-en-factoren/ingrijpende-gevolgen"),
-    new BronItem(
-        "Van Andel, H. W. H., Van Balkom, A. J. L. M., Bloks, J. A., Van den Brink, C., Canton, W. J., Griffioen, J. G., De Gruijl, I. P. I., Jacobs, C. G. A. M., De Jong, C. A. J., Kat, M. G., Kisjes, H., Kooij, J. J. S., Kortmann, F. A. M., Van der Leest-Tijmense, Y. G., Leidelmeijer, P., Maassen, N., Maduro, S. L., Oortwijn, A. J., Peeters, S., . . . Wieland, J. (2016). Psychiatrie voor de sociaal werker. In Bohn Stafleu van Loghum eBooks. Bohn Stafleu van Loghum. https://doi.org/10.1007/978-90-368-1247-4"
-    ),
     new BronItem("Visie & missie van Buro3o | Buro3o. (z.d.). Buro3o. Geraadpleegd op 19 mei 2026, van https://www.buro3o.nl/buro3o/visie/"),
     new BronItem(" Watzlawick, P., Beavin, J. H., & Jackson, D. D. (1975). De pragmatische aspecten van de menselijke communicatie. http://lib.ugent.be/en/catalog/rug01:000025500")
 ]);
